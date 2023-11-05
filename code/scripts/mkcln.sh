9eff66b496d8b336ce626baf1ae0c027695ab61d
Karl Wolf <karl.wolf2706@gmail.com>
2023-11-05 12:20:09 +0100

    renaming

--- code/scripts/mkcln.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for make in app/*/makefile sat/*/makefile
-do
-  make -C `dirname $make` clean
-done
-echo "mkcln.sh: Entering directory '`pwd`/bin'"
-cd bin/ || exit 1
-echo "rm -f \`cat .gitignore\`"
-rm -f `cat .gitignore`
